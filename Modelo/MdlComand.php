580015986a1dc9901d62242cbb659a58a3c61f99
AeroMobileSoft Colombia <gerencia@aeromobilesoft.com.co>
2020-01-14 16:08:03 -0500

    nota
    Inicio del proyecto

---
+++ Modelo/MdlComand.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class MdlComand {
+    //put your code here
+}
+
+?>
---
+++ Vistas/Index.php
@@ -0,0 +1,14 @@
+<?php
+    include_once './MasterPage/Header.php';
+?>
+
+    <div id="content-wrapper">
+        <h1>
+            Hola tarola
+        </h1>
+    </div>
+
+<?php
+    include_once './MasterPage/Footer.php';
+?>
+
